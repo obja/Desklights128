1cda3ef2e48a3b5dfe61771158f0a77c57185e4a
Alec Harley <alec.91@gmail.com>
2014-06-04 16:08:32 -0700

    Update to add Adafruit_GFX functions
    Add ability to draw characters and lines, circles, rectangles, triangles
    Set the cursor (default 1,0) and then stripwrite(char)

--- DeskLights128.ino
+++ DeskLights128.ino
@@ -45,6 +45,8 @@
 #include "Ethernet.h"
 #include "WebServer.h"
 #include <Adafruit_WS2801.h>
+#include "Adafruit_GFX.h"
+#include "glcdfont.c"
 
 /*** This is what you will almost certainly have to change ***/
 
@@ -66,6 +68,15 @@ uint16_t max_y = 8;
 Adafruit_WS2801 strip = Adafruit_WS2801(max_x, max_y, dataPin, clockPin, WS2801_RGB); // setting max_x and max_y here lets us use draw functions
 int defaultPattern = 0;
 
+//ada gfx vars
+int16_t cursor_x = 1;
+int16_t cursor_y = 0;
+uint8_t textsize = 1;
+uint16_t textcolor = Color(255,255,255);
+uint16_t textbgcolor = Color(0,0,0);
+boolean wrap = false;
+int16_t _width = max_x;
+int16_t _height = max_y;
 
 // 'graph' style x,y where 0,0 is bottom left
 
@@ -313,6 +324,126 @@ void p_cylon() {
   }
 
 }
+ //adafruit_gfx addons
+void stripwrite(uint8_t c) {
+  if (c == '\n') {
+    cursor_y += textsize*8;
+    cursor_x = 0;
+  }
+  else if (c == '\r') {
+    //skip
+  }
+  else {
+    drawChar(cursor_x, cursor_y, c, textcolor, textbgcolor, textsize);
+    cursor_x += textsize*6;
+    if(wrap && (cursor_x > (_width - textsize*6))) {
+      cursor_y += textsize*8;
+      cursor_x = 0;
+    }
+  }
+}
+
+void drawFastVLine(int16_t x, int16_t y,
+				 int16_t h, uint16_t color) {
+  // Update in subclasses if desired!
+  drawLine(x, y, x, y+h-1, color);
+}
+
+void drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+  strip.setPixelColor(g2p(x,y), color);
+}
+
+void drawLine(int16_t x0, int16_t y0,
+			    int16_t x1, int16_t y1,
+			    uint16_t color) {
+  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+  if (steep) {
+    swap(x0, y0);
+    swap(x1, y1);
+  }
+
+  if (x0 > x1) {
+    swap(x0, x1);
+    swap(y0, y1);
+  }
+
+  int16_t dx, dy;
+  dx = x1 - x0;
+  dy = abs(y1 - y0);
+
+  int16_t err = dx / 2;
+  int16_t ystep;
+
+  if (y0 < y1) {
+    ystep = 1;
+  } else {
+    ystep = -1;
+  }
+
+  for (; x0<=x1; x0++) {
+    if (steep) {
+      drawPixel(y0, x0, color);
+    } else {
+      drawPixel(x0, y0, color);
+    }
+    err -= dy;
+    if (err < 0) {
+      y0 += ystep;
+      err += dx;
+    }
+  }
+}
+
+
+void fillRect(int16_t x, int16_t y, int16_t w, int16_t h,
+			    uint16_t color) {
+  // Update in subclasses if desired!
+  for (int16_t i=x; i<x+w; i++) {
+    drawFastVLine(i, y, h, color);
+  }
+}
+
+void drawChar(int16_t x, int16_t y, unsigned char c,
+			    uint16_t color, uint16_t bg, uint8_t size) {
+
+  if((x >= _width)            || // Clip right
+     (y >= _height)           || // Clip bottom
+     ((x + 6 * size - 1) < 0) || // Clip left
+     ((y + 8 * size - 1) < 0))   // Clip top
+    return;
+
+  for (int8_t i=0; i<6; i++ ) {
+    uint8_t line;
+    if (i == 5) 
+      line = 0x0;
+    else 
+      line = pgm_read_byte(font+(c*5)+i);
+    for (int8_t j = 0; j<8; j++) {
+      if (line & 0x1) {
+        if (size == 1) // default size
+          drawPixel(x+i, y+j, color);
+        else {  // big size
+          fillRect(x+(i*size), y+(j*size), size, size, color);
+        } 
+      } else if (bg != color) {
+        if (size == 1) // default size
+          drawPixel(x+i, y+j, bg);
+        else {  // big size
+          fillRect(x+i*size, y+j*size, size, size, bg);
+        }
+      }
+      line >>= 1;
+    }
+  }
+}
+
+
+void setCursor(int16_t x, int16_t y) { //set text upper left point
+  cursor_x = x;
+  cursor_y = y;
+}
+
 
 // begin web handlers
 
